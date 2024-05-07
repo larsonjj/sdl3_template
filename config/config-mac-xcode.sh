8843657ea70159a2ed52cb65691aae2fe3233a91
Jake Larson <jake.j.larson@gmail.com>
2024-05-07 00:55:02 -0500

    chore: Update Xcode version to 15.4 and copy assets to build directory for macOS build

--- config/config-mac-xcode.sh
+++ config/config-mac-xcode.sh
@@ -4,6 +4,11 @@ cd ..
 
 mkdir -p build
 mkdir -p build/mac
+
+# Ensure asset folder is copied
+rm -rf build/mac/assets || true
+cp -R assets build/mac/assets
+
 cd build/mac
 cmake -G "Xcode" cmake -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=Darwin -DSDL_STATIC=ON -DSDL_SHARED=OFF ../..
-cmake --build . --parallel
+cmake --build . --config Release
