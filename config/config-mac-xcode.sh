1cf398604f4fbf24bc6c4d05d3b151326ba77d34
Jake Larson <jake.j.larson@gmail.com>
2024-05-07 16:09:01 -0500

    Refactor build workflow for macOS to improve asset copying

--- config/config-mac-xcode.sh
+++ config/config-mac-xcode.sh
@@ -7,8 +7,9 @@ mkdir -p build/mac
 
 cd build/mac
 cmake -G "Xcode" cmake -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=Darwin -DSDL_STATIC=ON -DSDL_SHARED=OFF ../..
-cmake --build . --config Release --parallel
+# cmake --build . --config Release --parallel
+cmake --build . --target install --config Release --parallel
 
 # Ensure asset folder is copied
-rm -rf Release/sdl_template.app/Contents/MacOS/assets || true
-cp -R ../../assets Release/sdl_template.app/Contents/MacOS/assets
+rm -rf install/sdl_template.app/Contents/MacOS/assets || true
+cp -R ../../assets install/sdl_template.app/Contents/MacOS/assets
