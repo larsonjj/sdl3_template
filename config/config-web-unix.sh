f74f0b12ab6b8b5f39f5ff85a6bf7f7b68d343aa
Jake Larson <jake.j.larson@gmail.com>
2024-05-06 20:17:04 -0500

    Fix web builds

--- config/config-web-unix.sh
+++ config/config-web-unix.sh
@@ -4,5 +4,11 @@ cd ..
 
 mkdir -p build
 mkdir -p build/web
+
+# Ensure asset folder is copied
+rm -rf build/web/assets || true
+cp -R assets build/web/assets
+
 cd build/web
-emcmake cmake ../..  && make
+emcmake cmake ../..
+cmake --build . --parallel
--- config/config-web-win.bat
+++ config/config-web-win.bat
@@ -2,5 +2,11 @@
 cd ..
 mkdir build
 mkdir build\web
+
+# Ensure asset folder is copied
+rd /s /q build\web\assets
+xcopy /E /I assets build\web\assets
+
 cd build\web
 emcmake cmake ..\..
+cmake --build . --parallel
