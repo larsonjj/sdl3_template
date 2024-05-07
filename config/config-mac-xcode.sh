1c754080326b66d343997ee4d8d9c94d43b643cb
Jake Larson <jake.j.larson@gmail.com>
2024-05-06 22:18:23 -0500

    chore: Update Xcode configuration and Info.plist for macOS build

--- config/config-mac-xcode.sh
+++ config/config-mac-xcode.sh
@@ -5,4 +5,5 @@ cd ..
 mkdir -p build
 mkdir -p build/mac
 cd build/mac
-cmake -G "Xcode" ../..
+cmake -G "Xcode" cmake -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=Darwin -DSDL_STATIC=ON -DSDL_SHARED=OFF ../..
+cmake --build . --parallel
