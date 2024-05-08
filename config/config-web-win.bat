ac0b578edfa3bf016f15d1f1fc76a00756526d1f
Jake Larson <jake.j.larson@gmail.com>
2024-05-08 03:48:05 -0500

    Refactor config-web-win.bat to improve build process and error handling

--- config/config-web-win.bat
+++ config/config-web-win.bat
@@ -1,12 +1,19 @@
 @echo OFF
-cd ..
-mkdir build
-mkdir build\web
+pushd ..
+IF NOT EXIST build mkdir build
+IF NOT EXIST build\web mkdir build\web
 
-# Ensure asset folder is copied
+:: Ensure asset folder is copied
 rd /s /q build\web\assets
+IF %ERRORLEVEL% NEQ 0 echo "Failed to remove assets" && exit /b
 xcopy /E /I assets build\web\assets
+IF %ERRORLEVEL% NEQ 0 echo "Failed to copy assets" && exit /b
 
-cd build\web
+pushd build\web
 emcmake cmake ..\..
+IF %ERRORLEVEL% NEQ 0 echo "Failed to run emcmake" && exit /b
+timeout /t 2 /nobreak > NUL
 cmake --build . --parallel
+IF %ERRORLEVEL% NEQ 0 echo "Failed to build" && exit /b
+popd
+popd
