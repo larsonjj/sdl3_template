e243166ebefc114d15302e6efe9d43c30172c1fd
Jake Larson <jake.j.larson@gmail.com>
2024-05-09 10:56:46 -0500

    Refactor build workflow to use correct build directory path

--- config/config-mac-xcode.sh
+++ config/config-mac-xcode.sh
@@ -7,9 +7,6 @@ mkdir -p build/mac
 
 cd build/mac
 cmake -G "Xcode" -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=Darwin -DSDL_STATIC=ON -DSDL_SHARED=OFF -S ../.. -B ./build
-echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-pwd
-ls
 cmake --build ./build --target install --config Release --parallel
 cpack -G DragNDrop -C Release
 
