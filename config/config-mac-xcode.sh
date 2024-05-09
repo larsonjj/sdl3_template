59231e3d4593e0ba73033f0939af024ab8f558e4
Jake Larson <jake.j.larson@gmail.com>
2024-05-09 11:58:30 -0500

    Refactor build workflow to exclude logo.png from macOS bundle

--- config/config-mac-xcode.sh
+++ config/config-mac-xcode.sh
@@ -8,8 +8,6 @@ mkdir -p build/mac
 cd build/mac
 cmake -G "Xcode" -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=Darwin -DSDL_STATIC=ON -DSDL_SHARED=OFF -S ../..
 cmake --build ./ --target install --config Release --parallel
-pwd
-ls
 cpack -G DragNDrop -C Release
 
 # Ensure asset folder is copied
