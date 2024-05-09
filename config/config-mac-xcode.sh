be12fea30cfa2d19ecc8714a4899a26eecb4c5ea
Jake Larson <jake.j.larson@gmail.com>
2024-05-09 13:00:40 -0500

    Refactor build workflow to use correct build directory path and Release configuration

--- config/config-mac-xcode.sh
+++ config/config-mac-xcode.sh
@@ -9,7 +9,3 @@ cd build/mac
 cmake -G "Xcode" -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=Darwin -DSDL_STATIC=ON -DSDL_SHARED=OFF -S ../..
 cmake --build ./ --target install --config Release --parallel
 cpack -G DragNDrop -C Release
-
-# Ensure asset folder is copied
-# rm -rf .build/sdl-template.app/Contents/MacOS/assets || true
-# cp -R ../../assets ./build/sdl-template.app/Contents/MacOS/assets
