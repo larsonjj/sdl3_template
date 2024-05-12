3f45009676ec4485e06a239175490dfd787b6f28
Jake Larson <jake.j.larson@gmail.com>
2024-05-12 15:44:30 -0500

    Simply copying assets to build folders

--- config/config-web-unix.sh
+++ config/config-web-unix.sh
@@ -5,10 +5,6 @@ cd ..
 mkdir -p build
 mkdir -p build/web
 
-# Ensure asset folder is copied
-rm -rf build/web/assets || true
-cp -R assets build/web/assets
-
 cd build/web
 emcmake cmake ../..
 cmake --build . --parallel
--- config/config-web-win.bat
+++ config/config-web-win.bat
@@ -3,12 +3,6 @@ pushd ..
 IF NOT EXIST build mkdir build
 IF NOT EXIST build\web mkdir build\web
 
-:: Ensure asset folder is copied
-rd /s /q build\web\assets
-IF %ERRORLEVEL% NEQ 0 echo "Failed to remove assets" && exit /b
-xcopy /E /I assets build\web\assets
-IF %ERRORLEVEL% NEQ 0 echo "Failed to copy assets" && exit /b
-
 pushd build\web
 emcmake cmake ..\..
 IF %ERRORLEVEL% NEQ 0 echo "Failed to run emcmake" && exit /b
