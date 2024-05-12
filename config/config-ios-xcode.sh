0b456d4871da18239e2459eaebda89d2af75328d
Jake Larson <jake.j.larson@gmail.com>
2024-05-11 21:07:07 -0500

    Refactor build workflow to include iOS builds

--- config/config-ios-xcode.sh
+++ config/config-ios-xcode.sh
@@ -5,4 +5,4 @@ cd ..
 mkdir -p build
 mkdir -p build/ios
 cd build/ios
-cmake -G "Xcode" -DCMAKE_SYSTEM_NAME="iOS" ../..
+cmake -G "Xcode" -DCMAKE_SYSTEM_NAME="iOS" -DCMAKE_OSX_SYSROOT=iphone -DCMAKE_OSX_ARCHITECTURES=armv64 ../..
---
+++ config/config-iossimulator-xcode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "${0%/*}"
+cd ..
+
+mkdir -p build
+mkdir -p build/ios
+cd build/ios
+cmake -G "Xcode" -DCMAKE_SYSTEM_NAME="iOS" -DCMAKE_OSX_SYSROOT=iphonesimulator -DCMAKE_OSX_ARCHITECTURES=x86_64 ../..
