ec224ac8c61ad41ac50d17321c6d6986f99ed2f5
Jake Larson <jake.j.larson@gmail.com>
2024-05-15 01:12:30 -0500

    chore: Update CMakeLists.txt and build configuration for iOS targets

--- config/config-ios-xcode.sh
+++ config/config-ios-xcode.sh
@@ -6,4 +6,4 @@ mkdir -p build
 mkdir -p build/ios
 cd build/ios
 
-cmake -G "Xcode" -DCMAKE_SYSTEM_NAME="iOS" -DCMAKE_OSX_SYSROOT=iphone -DCMAKE_OSX_ARCHITECTURES=armv64 -S ../..
+cmake -G "Xcode" -DPLATFORM="iOS" -DCMAKE_SYSTEM_NAME="iOS" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_OSX_SYSROOT=iphoneos -DCMAKE_OSX_ARCHITECTURES=arm64 -S ../..
