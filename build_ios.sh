f272dd5fd9ba86db2bc15841bd24a5c13b26c105
Jake Larson <jake.j.larson@gmail.com>
2025-02-01 00:16:30 -0600

    Add iOS build script and update .gitignore for CMake files

---
+++ build_ios.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create output directory for iOS builds
+mkdir -p build/ios
+
+cmake -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_SYSROOT=iphoneos -S . -B build/ios
+
+cmake --build build/ios --target sdl-template --config Release
