982af3db59c21c3fb68f795460dbdc82e2425bbf
Jake Larson <jake.j.larson@gmail.com>
2025-02-05 22:36:44 -0600

    Update iOS build script to use Debug configuration and modify Android CMake arguments for improved asset handling

--- build_ios.sh
+++ build_ios.sh
@@ -5,4 +5,4 @@ mkdir -p build/ios
 
 cmake -G "Xcode" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_SYSROOT=iphoneos -S . -B build/ios
 
-cmake --build build/ios --target sdl3_template --config Release
+cmake --build build/ios --target sdl3_template --config Debug
